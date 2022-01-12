340c2bc45de04fc28cfc6fcdcfbed813443e5329
Андрей Шестаков <GlobalShow6@Gmail.com>
2022-01-12 21:30:00 +0300

    Урок 13: Изучение фреймфорка Express

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 var fs = require('fs');
-
-//Подключение к локальному серверу.
+var express = require('express');
 var http = require('http');
 
+var app = express();
+
+//Подключение к локальному серверу.
+
+app.get('/', function(req, res){
+  res.send('This is home');
+});
+
+app.get('/news', function(req, res){
+  res.send('This is news');
+});
+
+app.get('/news/:id', function(req, res) {
+  res.send('ID is - ' + req.params.id);
+});
+app.listen(3000);
+
+/*
 var server = http.createServer(function(req, res){
   console.log("URL страницы: " + req.url);
   if(req.url === '/index' || req.url === '/')  {
@@ -17,11 +34,12 @@ var server = http.createServer(function(req, res){
     res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
     fs.createReadStream(__dirname + '/404.html').pipe(res);
   }
-  /*
+
   var myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf8');
   myReadShort.pipe(res);
-  */
+
 })
 
 server.listen(3000, '127.0.0.1');
 console.log("Мы отслеживаем порт 3000");
+*/
