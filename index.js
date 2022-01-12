7dbd7b6c62d90ce0a1af4246629299327a3bcb97
Андрей Шестаков <GlobalShow6@Gmail.com>
2022-01-12 18:16:01 +0300

    Урок 5: Создание и отследивание событий в Node.js

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-var things = require('./things');
-console.log(things.some_value);
-console.log(things.array_counter([1, 7, 99, 8, 45, 8]));
-console.log(things.multiply(5, 8));
+var events = require('events');
+var util = require('util');
+
+/*
+{
+var myEmit = new events.EventEmitter();
+
+myEmit.on('some_event', function(text){
+  console.log(text);
+} );
+
+myEmit.emit('some_event', 'Обработчик событий сработал!');
+}
+*/
+
+var Cars = function(model) {
+  this.model = model;
+};
+
+util.inherits(Cars, events.EventEmitter);
+
+var bmw = new Cars('BMW');
+var audi = new Cars('Audi');
+var volve = new Cars('Volve');
+
+var cars = [bmw, audi, volve];
+cars.forEach(function(car) {
+  car.on('speed', function(text) {
+    console.log(car.model + " speed is - " + text);
+  });
+});
+
+bmw.emit('speed', '254.5 km');
+volve.emit('speed', '180 km');
