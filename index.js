82a2861afb2322ed713847ddc0eb9f264bc31e66
Андрей Шестаков <GlobalShow6@Gmail.com>
2022-01-12 20:36:25 +0300

    Урок 11: Маршрутизация Node JS

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ var http = require('http');
 
 var server = http.createServer(function(req, res){
   console.log("URL страницы: " + req.url);
-  res.writeHead(200, {'Content-Type': 'application/json; charset=utf-8'});
-  var obj = {
-    model: 'Audi',
-    speed: '234.5',
-    wheels: 4
+  if(req.url === '/index' || req.url === '/')  {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
   }
-  res.end(JSON.stringify(obj))
+  else if (req.url === '/about') {
+    res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/about.html').pipe(res);
+  }
+  else {
+    res.writeHead(404, {'Content-Type': 'text/html; charset=utf-8'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
 
   /*
   var myReadShort = fs.createReadStream(__dirname + '/index.html', 'utf8');
